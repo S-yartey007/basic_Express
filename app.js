f4667662a8480df995da1db368aa133ac2857aa8
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-08-19 21:01:52 +0000

    Express intialised

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=> res.send("hello, world"));
+
+const PORT = 3000;
+
+app.listen(PORT,() => console.log(`Express app listening at port ${PORT}`))
